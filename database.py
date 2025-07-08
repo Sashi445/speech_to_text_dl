eee23ac090cfc6f303af2159dee71db01ba20471
charan Kumar <charan.ku1605@gmail.com>
2025-07-07 22:43:36 -0400

    text translation

--- database.py
+++ database.py
@@ -1,12 +1,12 @@
-# # database.py
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import sessionmaker, scoped_session
-# from sqlalchemy.ext.declarative import declarative_base
+# database.py
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.ext.declarative import declarative_base
 
-# # SQLite engine (can replace with PostgreSQL later)
-# SQLALCHEMY_DATABASE_URL = "sqlite:///caption_jobs.db"
+# SQLite engine (can replace with PostgreSQL later)
+SQLALCHEMY_DATABASE_URL = "sqlite:///caption_jobs.db"
 
-# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
-# SessionLocal = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 
-# Base = declarative_base()
+Base = declarative_base()
